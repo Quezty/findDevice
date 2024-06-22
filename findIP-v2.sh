d9bff54ac3f2c31707bb3886f67fab16ae99c0e4
Quezty <storlijoachim@gmail.com>
2024-06-22 03:13:08 +0200

    did something I think

--- findIP-v2.sh
+++ findIP-v2.sh
@@ -1,8 +1,8 @@
-# !/bin/bash
+#!/bin/bash
 
 echo "What do you wish to connect to?"
 
-read -p "laptop wi-fi, laptop ethernet or desktop ethernet (lw, le, de)" device
+read -p -r "laptop wi-fi, laptop ethernet or desktop ethernet (lw, le, de)" device
 
 case "$device" in
   "lw") 
@@ -19,9 +19,19 @@ case "$device" in
     ;;
 esac
 
-find_device (MAC_ADDRESS) {
+find_device () {
   network_range="10.0.0.0/24" 
   scan_results=$(sudo nmap -sn --send-eth $network_range)
   processed_results=$(echo "$scan_results" | awk '/Nmap scan report/{ip=$NF}/MAC Address:/{print ip, $3}')
-
+  found=0 
+  while IFS= read -r line; do
+    if [[ $line == *$1* ]]; then
+        found=1
+        ip_address=$(echo "$line" | awk '{print $1}')
+         
+    fi
+  done <<< "$processed_results"
+  if [[ $found -eq 0 ]]; then
+    echo "Device with MAC address $1 not found."
+  fi
 } 
